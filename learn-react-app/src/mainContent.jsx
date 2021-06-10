c922fdb2c1c003881de02608f937f0ec566055bb
sakthikarthickraja98 <60427728+sakthikarthickraja98@users.noreply.github.com>
2021-06-10 17:45:33 +0530

    add table in main

--- learn-react-app/src/mainContent.jsx
+++ learn-react-app/src/mainContent.jsx
@@ -3,18 +3,51 @@ import React,{Component} from "react";
 export default class MainContent extends Component{
   state = {
       pagetitle : "Blood-donate",
-      donerscount : 160
+      donarscount : 160,
+      donardetails:[
+          {id:1,name:"Kadir",phone:923456789,address:{city:"Chennai"}},
+          {id:2,name:"Anand",phone:946456789,address:{city:"Madurai"}},
+          {id:3,name:"Sankar",phone:963456789,address:{city:"Coimbatore"}},
+          {id:4,name:"Ram",phone:985456789,address:{city:"Banglore"}},
+          {id:5,name:"KKumar",phone:923456789,address:{city:"Pune"}},
+      ]
   };
 
   render(){
     return (
     <div>
      <h4 className="border-bottom m-1 p-1">{this.state.pagetitle}
-     <span className="badge-secondary m-2">{this.state.donerscount}</span>
+     <span className="badge-secondary m-2">{this.state.donarscount}</span>
      
      <button className="btn btn-info" onClick={this.onrefreshclick}>Refresh</button>
      
      </h4>
+
+     <table className="table">
+         <thead>
+            <tr>
+                <th>Doner ID</th>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>City</th>
+            </tr>
+         </thead>
+         <tbody>
+            {
+            this.state.donardetails.map((cust)=>{
+            return(   
+                 <tr key={cust.id}>
+                 <td>{cust.id}</td>
+                 <td>{cust.name}</td>
+                 <td>{cust.phone}</td>
+                 <td>{cust.address.city}</td>
+             </tr>
+            );
+            })
+            }
+             
+         </tbody>
+     </table>
      </div>
     );
   }
@@ -22,7 +55,7 @@ export default class MainContent extends Component{
   //excutes when user click on refresh button
   onrefreshclick = () => {
     this.setState({
-        donerscount:340
+        donarscount:340
     });
   }
 }
