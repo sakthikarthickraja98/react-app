98060a3a79fd5879f2b46a4778a6ae729866c324
sakthikarthickraja98 <60427728+sakthikarthickraja98@users.noreply.github.com>
2021-06-10 16:45:32 +0530

    refresh button

--- learn-react-app/src/mainContent.jsx
+++ learn-react-app/src/mainContent.jsx
@@ -2,12 +2,25 @@ import React,{Component} from "react";
 
 export default class MainContent extends Component{
   state = {
-      apptitle : "Blood-donate"
+      pagetitle : "Blood-donate",
+      donerscount : 160
   };
 
   render(){
     return (
-     <div className="border-bottom m-1 p-1">{this.state.apptitle}</div>
+    <div>
+     <h4 className="border-bottom m-1 p-1">{this.state.pagetitle}
+     <span className="badge-secondary m-2">{this.state.donerscount}</span>
+     
+     <button className="btn btn-info" onClick={this.onrefreshclick}>Refresh</button>
+     
+     </h4>
+     </div>
     );
+  }
+
+  //excutes when user click on refresh button
+  onrefreshclick(){
+      console.log("Refresh Clicked")
   }
 }
